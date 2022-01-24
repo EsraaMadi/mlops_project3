abe70669573e60126a081c43bd3d899cd3499f60
EsraaMadi <esraa.sabry.mohamed@gmail.com>
2022-01-24 23:31:42 +0300

    update

--- starter/test_model.py
+++ starter/test_model.py
@@ -13,7 +13,8 @@ from .ml.data import process_data
 @pytest.fixture
 def data():
     # load data
-    df = pd.read_csv("../data/census_clean_data.csv")
+#     df = pd.read_csv("../data/census_clean_data.csv")
+    df = pd.read_csv(os.path.join(os.getcwd(), "../data/census_clean_data.csv"))
    
     # load model
     model = os.path.join(os.getcwd(), "../model/model.pkl")
